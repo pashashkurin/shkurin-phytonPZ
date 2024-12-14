0a7b050176906feb7788f971c0bc11eea29deb5b
pashashkurin <pashashkurin242@gmail.com>
2024-12-14 19:14:27 +0300

    Update PZ-3.2.py

--- PZ-3.2.py
+++ PZ-3.2.py
@@ -1,3 +1,8 @@
+'''
+Даны четыре целых числа, одно из которых отлично от трех других, равных между
+собой. Определить порядковый номер числа, отличного от остальных.
+'''
+
 numbers = []
 for i in range(4):
     num = int(input(f"Введите число {i + 1}: "))
@@ -6,9 +11,9 @@ for i in range(4):
 unique_index = -1
 
 for i in range(4):
-    # Если текущее число не равно двум другим, то оно уникально
+
     if numbers[i] != numbers[(i + 1) % 4] and numbers[i] != numbers[(i + 2) % 4]:
-        unique_index = i + 1  # Порядковый номер (начиная с 1)
+        unique_index = i + 1  
         break
 
 if unique_index != -1:
