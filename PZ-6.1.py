00604f14bc9eff7f68db706289f2c7abad0abab4
pashashkurin <pashashkurin242@gmail.com>
2025-02-15 15:43:05 +0300

    Create PZ-6.1.py

---
+++ PZ-6.1.py
@@ -0,0 +1,14 @@
+def increase_even_numbers(lst):
+    
+    first_even = next((x for x in lst if x % 2 == 0), None)
+
+  
+    if first_even is not None:
+        lst = [x + first_even if x % 2 == 0 else x for x in lst]
+    
+    return lst
+
+N = 10
+lst = [1, 3, 5, 2, 4, 6, 7, 8, 9, 10]
+result = increase_even_numbers(lst)
+print(result)
